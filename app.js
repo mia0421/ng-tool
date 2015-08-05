a0130315663426de139eba8d6519165f9ebdfbbc
陳品融 <joeschen0421@gmail.com>
2015-08-05 18:14:03 +0800

    1.建立側欄tool 2.建立main page

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ import angular from 'angular';
 let ngToolApp = angular.module('ngToolApp', ["ngToolSidebar"]);
 
 ngToolApp.controller('isCtrl', ['$scope', function ($scope) {
+	$scope.menu = [
+		{name:'ngToolSidebar', src:'',show:true,active: true},
+		{name:'分頁', src:'',show:true,active:false}
+
+	]
+
+
+
 	$scope.ngSidebar = {
 		 isSwitch:false,
 		 isDirection:'left',
